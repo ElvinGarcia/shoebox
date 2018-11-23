52fefcf7c4a715ee0183f2be435a767933819328
ElvinGarcia <dimix181@gmail.com>
2018-11-22 22:02:22 -0500

    update user model to user remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_acessor :remember_token
   #validates the email submitted format
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   #converts email to downcase  before saving it inorder to prevent inconsistency 
@@ -16,9 +17,22 @@ class User < ApplicationRecord
   #has_many 
   #has_many 
 
-
-
+# Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
   
-     
+  #returns a random url safe token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #retrieves the set token
+  def remember
+    self.remember_token = ...
+    update_attribute(:remember_digest, ...)
+  end
 
  end
