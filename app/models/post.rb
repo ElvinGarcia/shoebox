e2b57b79d5255a9f9127081b21008fe02acde7f0
ElvinGarcia <dimix181@gmail.com>
2019-04-08 21:45:44 -0400

    arranged post to load desc instead of asc

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
     has_many_attached :images, dependent: :destroy
     validates :description, presence: true
     validates :title, presence: true
+    default_scope {order(created_at: :desc)}
 
 end
