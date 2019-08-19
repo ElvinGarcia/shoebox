fce085a277735601e20f48d47f1c1cdf2d3d261a
ElvinGarcia <dimix181@gmail.com>
2019-08-18 23:01:06 -0400

    validations where added to the budget model

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,9 @@
 class Budget < ApplicationRecord
     define_attribute_methods 
     has_many :receipts , dependent: :destroy
+    validates :amount, numericality:{message:"%{value} is not a valid  %{model} %{attribute}"}
+    validates :category, presence:{uniqueness: true , message:"that %{attribute} has been already taken"}
+    
     
 
 
